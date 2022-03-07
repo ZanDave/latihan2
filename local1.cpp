5f91375712eee4e72bf5fe939aaaff2147fca5a9
ZanDave <david21007@mail.unpad.ac.id>
2022-03-07 15:34:31 +0700

    mengubah local1.cpp

--- local1.cpp
+++ local1.cpp
@@ -4,5 +4,6 @@ int main() {
     std::cout << "Ini adalah file cpp dalam repo saya\n";
     std::cout << "Saya menambah baris ini\n";
     std::cout << "hai hai";
+    std::cout << "hai lagi";
     return 0;
 }
