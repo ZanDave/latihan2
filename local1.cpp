7460c861285ebf0833e9de454f810595930577ef
ZanDave <david21007@mail.unpad.ac.id>
2022-03-07 15:31:32 +0700

    menambah local1.cpp

---
+++ local1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Ini adalah file cpp dalam repo saya\n";
+    std::cout << "Saya menambah baris ini\n";
+    std::cout << "hai hai";
+    return 0;
+}
